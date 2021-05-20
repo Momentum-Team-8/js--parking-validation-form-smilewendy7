8a020f9e030f6f58eca39caacabdb0a1ae51aaa3
smilewendy7 <smilewendy7@gmail.com>
2021-05-20 19:07:30 -0400

    # 4 kind of finishing ... only shows 1 sec..

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 console.log('Add validation!');
 
-//CVV must be a 3 digits number
+
 
 
 let form = document.querySelector("#parking-form"); 
 
 let formIsValid;
 
+let total = document.querySelector("#total"); 
+
 // test for input --- log 
 // form.addEventListener('input', event=> {
 //     console.log('user input captred!')
 // })
 
+
+
+//show the user toal cost is 5 div total
+// --- "the cost is 5 dollar perday"  if the form is valid
+
+
 form.addEventListener('submit', event=>{
-    event.PreventDefault()
+    // event.PreventDefault()
+
+    // if (formIsValid){
+
+        document.querySelector("#total").innerHTML = "The cost is $5 per day"
     
 
+    // }
+
+   
 
 
 }
 )
 
-
+// #6 
