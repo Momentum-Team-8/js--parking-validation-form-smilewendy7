1e532c6afce8f31ae97c7f234ad2fcea6fa5e6cf
smilewendy7 <smilewendy7@gmail.com>
2021-05-20 17:59:08 -0400

    #2 done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 console.log('Add validation!');
 
+//car year must be a number 
+
+// let carYear = document.querySelector('# car-year')
+
+    // car year must be after 1990
+
+
+//number of days must be a number
+
+// number of days must be between 1 and 30 
+
+
+
 
 
