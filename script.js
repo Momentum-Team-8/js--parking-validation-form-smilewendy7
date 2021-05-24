fc82a81ff1225276a08d420c244753d38a41a87d
smilewendy7 <smilewendy7@gmail.com>
2021-05-24 02:18:41 -0400

    v1 ---- fix the issue of showing error message after correct it

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ const nameInput = document.querySelector('#name')
 
 // error: field is required, then notificaiton / alert
 let error1 = document.createElement("div")
+error1.setAttribute('id', 'error1')
+
 let error2 = document.createElement("div")
+error2.setAttribute('id', 'error2')
 
 // allow to submit the form only when all fields are valid
 
@@ -42,8 +45,11 @@ form.addEventListener('submit', function (event) {
             // above if inputIsValid_name and inputIsValid_carYear both true...
             allFormsAreValid = true;
             console.log ('submitted!')
-
             // why this does not work? 
+
+            // show the cost is 5 dollar
+
+            document.querySelector('#total').innerHTML = "the cost is $5 per day"
             
         }
     }
@@ -61,42 +67,49 @@ form.addEventListener('submit', function (event) {
                else{ 
                 inputIsValid_name = true;
                 console.log('valid11') 
+                // remove the text added for invalid ..... 
+                if(document.querySelector('#error1')){
+                    document.querySelector('#error1').remove()
+                }
                }
             }
 
 
             
 
-            function validateInput_carYear() {
-            // carYear is number and >1990
+        function validateInput_carYear() {
+        // carYear is number and >1990
 
-                 //define local variables
-                const carYearInput = document.querySelector('#car-year');
-                if (carYearInput.value === "") {
+        //define local variables
+            const carYearInput = document.querySelector('#car-year');
+            console.log(carYearInput.value)
+
+            if (carYearInput.value === "") {
                     console.log('input invalid22') 
                        inputIsValid_carYear= false;
                        document.querySelector('#car-field').classList.add('input-invalid')
-                       document.querySelector('#car-field').appendChild(error2).innerHTML = "This field is required. "
-                       
-                    }
-                   else {
-                       if(carYearInput.value == 1 ){
-                           if(carYearInput.value >1990 && carYearInput.value < 2021){
-                            inputIsValid_carYear= true;
-                            console.log('valid22') 
-                           }
-                           else{
-                            document.querySelector('#car-field'). appendChild(error2).innerHTML = "year of car must be latter than 1990"
-                            }}
-
-                        else{
-                            document.querySelector('#car-field'). appendChild(error2).innerHTML = "must be a number"
+                       document.querySelector('#car-field').appendChild(error2).innerHTML = "This field is required. "     
+            }
+            else if(typeof(carYearInput.value) === "number"){
+                    console.log("car year is a number")
+                    if(carYearInput.value >= 1990 && carYearInput.value <= 2021){
+                        inputIsValid_carYear= true;
+                            console.log('valid22')
+                            if(document.querySelector('#error2')){
+                                document.querySelector('#error2').remove()
                             }
-
-                       }
                     }
+                    else{document.querySelector('#car-field'). appendChild(error2).innerHTML = "year of car must be latter than 1990"}
+            }   
+            else {
+                document.querySelector('#car-field'). appendChild(error2).innerHTML = "must be a number"
+            }                  
+        }            
                    
                 
+
+// Q4 ========  shoe total is 5 dollar when all valid and 
+//show "the cost is 5"
 
 
 
